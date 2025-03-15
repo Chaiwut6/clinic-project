17507048935b43b4cc2598088358015f0dd0f0ee
Chaiwut <chivwut0646@gmail.com>
2025-03-15 11:22:50 +0700

    index

--- server/index.js
+++ server/index.js
@@ -1,39 +1,39 @@
 const express = require('express');
-const path = require('path');
+const path = require("path");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
 const studentRoutes = require('./routes/studentRoutes');
 const doctorRoutes = require('./routes/doctorRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
-const managerRoutes = require('./routes/manRoutes');
+const mangerRoutes = require('./routes/manRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const uploadRoutes = require("./routes/uploadRoutes"); 
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-
+const allowedOrigins = [
+  "https://clinic-project-w900.onrender.com"  
+];
 // Middleware
 app.use(express.json());
+const uploadDir = path.join(__dirname, "uploads");
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: ["https://clinic-project-w900.onrender.com"]
+  origin: allowedOrigins,
+  methods: "GET,POST,PUT,DELETE"
 }));
 
-// กำหนด Route สำหรับ API
+app.use("/uploads", express.static(uploadDir));
+
+// Routes
 app.use('/api/students', studentRoutes);
 app.use('/api/doctors', doctorRoutes);
 app.use('/api/employees', employeeRoutes);
-app.use('/api/manager', managerRoutes);
+app.use('/api/manager', mangerRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/upload', uploadRoutes);
-
-// สร้าง route เพื่อให้สามารถเข้าถึงหน้า index.html ได้
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/view/index.html'));
-});
 
 // Start server
 app.listen(port, () => {
